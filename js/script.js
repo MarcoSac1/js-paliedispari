05ebc4117956ab217d9584b43ec9f125f2f98867
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-16 16:20:58 +0200

    created function to generate random number

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 // creo una variabile dove faccio inserire al utente una parola 
 
 const userWord = prompt('type a word');
+// console.log(userWord);
 
-console.log(userWord);
+
+
+
 // creo una 'function' per verificare se la parola e dispari o pari
-function palindroma(pari , dispari) {
-    
-    if( userWord % 2 === 0 ){
-        console.log('pari');
-    }else{
-        console.log('dispari');
+function palindroma() {
+    if(userWord % 2 === 0) {
+        console.log(pari);
+    }else if(userWord % 2 === 1) {
+        console.log(dispari);
     }
+    
+    return palindroma;
+}
+
+
+
+// genero un numero randomico
+let numeroRandomico = getRandomInt (1 ,5);
+
+function getRandomInt(min, max) {
+    const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    console.log(randomNumber);
+    return randomNumber;
 }

8f30e7aaca9245cebeebdbeda30e5d4acb1ea7f7
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-17 15:44:22 +0200

    created while circle

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,7 +1,15 @@
 
 
-// // genero un numero randomico
-// let numeroRandomico = getRandomInt (1 ,5);
+// - creo una variabile dove faccio scegliere al utente pari o dispari 
+let oddOrEven = prompt('pari o dispari');
+
+// con un ciclo dove controllo che l'utene inseririsca correttamente 'pari o dispari'
+while(oddOrEven !== 'pari' && oddOrEven !== 'dispari'){
+    let oddOrEven = prompt('pari o dispari');
+}
+
+// - faccio inserire un numero da 1 a 5
+
 
 
 
@@ -10,3 +18,11 @@ function getRandomInt(min, max) {
     const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
     return randomNumber;
 }
+
+
+
+
+
+
+// - creo una function che genera un numero randomico da 1 a 5
+// - creo una funzione che controlla se il numero ricevuto e pari o dispari

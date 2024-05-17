c954169879ce9ebeec34ed19ac83cf4f2cfd0e60
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-17 15:59:26 +0200

    created while loop for  number chose

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,23 +1,25 @@
 
 
 // - creo una variabile dove faccio scegliere al utente pari o dispari 
-let oddOrEven = prompt('pari o dispari');
+let oddOrEven = prompt('pari o dispari').trim().toLocaleLowerCase();
 
 // con un ciclo dove controllo che l'utene inseririsca correttamente 'pari o dispari'
 while(oddOrEven !== 'pari' && oddOrEven !== 'dispari'){
-    let oddOrEven = prompt('pari o dispari');
+    oddOrEven = prompt('pari o dispari').trim().toLocaleLowerCase();
 }
 
-// - faccio inserire un numero da 1 a 5
-
-
-
+// - chiedo al utente di inserire un numero e stampo il risultato in console
+let userNumber = Number.parseInt(prompt('scegli un numero da 1 a 5'), 10);
+while(Number.isNaN(userNumber)|| userNumber < 1 || userNumber > 5){
+    userNumber = Number.parseInt(prompt('scegli un numero da 1 a 5'), 10);
+}
+console.log(`L'utente ha scelto ${oddOrEven}, e il numero ${userNumber}`);
 
 //  creo una funzione che genera un numero tra minimo e massimo 
-function getRandomInt(min, max) {
-    const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-    return randomNumber;
-}
+// function getRandomInt(min, max) {
+//     const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+//     return randomNumber;
+// }
 
 
 
